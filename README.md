b17efb0e044c8f31fe94d3ff80ef9b38b15538d5
Raadsl <83394320+Raadsl@users.noreply.github.com>
2023-06-19 06:18:24 +0000

    last small changes, deploying still fails

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ReplitNotes
 
 Keep track with what you still need to do! (probably a lot). ReplitNotes is a sequel to the Todo Extension from me. It has user-wide code snippets, a nicer UI, works better, and doesn't create files in your repl. The data gets stored outside of your repl. That's wayy better before, and doesn't take up any bytes for you!\n\nPlease don't store sensitive information since it's not 100% private. It does gets hashed so I don't know who made the note.
+
+Basically done but ![Error](https://R.aadsel.repl.co/i/dviOeA.png)
+getting that error when deploying. Doesn't say anything in logs
--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
 from datetime import datetime
 import hashlib
+from random import randint
 
 
 app = Flask(__name__)
-CORS(app, origins=["https://replit.com"])
+CORS(app, origins=["*"])
 
 app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
@@ -19,25 +20,25 @@ socketio = SocketIO(app, cors_allowed_origins="*", allow_unsafe_werkzeug=True)
 
 
 def hash_user_slug(user_slug, salt):
-    hasher = hashlib.sha256()
-    hasher.update((user_slug + salt).encode('utf-8'))
-    hashed_slug = hasher.hexdigest()
-    return hashed_slug
+  hasher = hashlib.sha256()
+  hasher.update((user_slug + salt).encode('utf-8'))
+  hashed_slug = hasher.hexdigest()
+  return hashed_slug
 
 USER_SLUG_SALT = os.environ["SECRET_SALT"]
 
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_slug = db.Column(db.String(80), nullable=False)
-    note_content = db.Column(db.String, nullable=False)
-    done = db.Column(db.Boolean, default=False)
-  
+  id = db.Column(db.Integer, primary_key=True)
+  user_slug = db.Column(db.String(80), nullable=False)
+  note_content = db.Column(db.String, nullable=False)
+  done = db.Column(db.Boolean, default=False)
+
 class Snippet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_slug = db.Column(db.String(80), nullable=False)
-    title = db.Column(db.String, nullable=False)
-    code = db.Column(db.String, nullable=False)
-    last_used = db.Column(db.DateTime, default=datetime.utcnow) 
+  id = db.Column(db.Integer, primary_key=True)
+  user_slug = db.Column(db.String(80), nullable=False)
+  title = db.Column(db.String, nullable=False)
+  code = db.Column(db.String, nullable=False)
+  last_used = db.Column(db.DateTime, default=datetime.utcnow) 
 
 @app.route('/')
 def index():
@@ -49,186 +50,186 @@ def manifest():
 
 @socketio.on('connect')
 def handle_connect():
-    emit('connected', {'data': 'Connected'})
+  emit('connected', {'data': 'Connected'})
 
 
 @socketio.on('store_note')
 def handle_store_note(data):
-    user_slug = data.get('user_slug')
-    note = data.get('note')
+  user_slug = data.get('user_slug')
+  note = data.get('note')
 
-    if user_slug and note:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        encoded_note = base64.b64encode(note.encode('utf-8')).decode('utf-8') # base64 isn't for security but incase someone injects some crypto url and the repl will freeze by replit;
-        new_note = Note(user_slug=hashed_slug, note_content=encoded_note)
-        db.session.add(new_note)
-        db.session.commit()
-        emit('note_stored', {'message': 'Note stored successfully'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+  if user_slug and note:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      encoded_note = base64.b64encode(note.encode('utf-8')).decode('utf-8') # base64 isn't for security but incase someone injects some crypto url and the repl will freeze by replit;
+      new_note = Note(user_slug=hashed_slug, note_content=encoded_note)
+      db.session.add(new_note)
+      db.session.commit()
+      emit('note_stored', {'message': 'Note stored successfully'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('get_notes')
 def handle_get_notes(data):
-    user_slug = data.get('user_slug')
-    
-    if user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        notes = Note.query.filter_by(user_slug=hashed_slug).all()
+  user_slug = data.get('user_slug')
+  
+  if user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      notes = Note.query.filter_by(user_slug=hashed_slug).all()
 
-       
-        notes_list = [{
-            'id': note.id,
-            'note': base64.b64decode(note.note_content.encode('utf-8')).decode('utf-8'),
-            'done': note.done
-        } for note in notes]
-        emit('notes_fetched', notes_list)
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+     
+      notes_list = [{
+          'id': note.id,
+          'note': base64.b64decode(note.note_content.encode('utf-8')).decode('utf-8'),
+          'done': note.done
+      } for note in notes]
+      emit('notes_fetched', notes_list)
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('delete_note')
 def handle_delete_note(data):
-    note_id = data.get('note_id')
-    user_slug = data.get('user_slug')
+  note_id = data.get('note_id')
+  user_slug = data.get('user_slug')
 
-    if note_id and user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        note = Note.query.filter_by(id=note_id, user_slug=hashed_slug).first()
+  if note_id and user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      note = Note.query.filter_by(id=note_id, user_slug=hashed_slug).first()
 
-        if note:
-            db.session.delete(note)
-            db.session.commit()
-            emit('note_deleted', {'message': f'Note with ID {note_id} has been deleted'})
-        else:
-            emit('error_occurred', {'message': f'Note with ID {note_id} not found or not owned by the user'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+      if note:
+          db.session.delete(note)
+          db.session.commit()
+          emit('note_deleted', {'message': f'Note with ID {note_id} has been deleted'})
+      else:
+          emit('error_occurred', {'message': f'Note with ID {note_id} not found or not owned by the user'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('mark_note')
 def handle_mark_note(data):
-    note_id = data.get('note_id')
-    is_done = data.get('is_done')
-    user_slug = data.get('user_slug')
-    
-    if note_id is not None and is_done is not None and user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        note = Note.query.filter_by(id=note_id, user_slug=hashed_slug).first()
+  note_id = data.get('note_id')
+  is_done = data.get('is_done')
+  user_slug = data.get('user_slug')
+  
+  if note_id is not None and is_done is not None and user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      note = Note.query.filter_by(id=note_id, user_slug=hashed_slug).first()
 
-        if note:
-            note.done = is_done
-            db.session.commit()
-            emit('note_marked', {'message': f'Note with ID {note_id} has been marked'})
-        else:
-            emit('error_occurred', {'message': f'Note with ID {note_id} not found or not owned by the user'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+      if note:
+          note.done = is_done
+          db.session.commit()
+          emit('note_marked', {'message': f'Note with ID {note_id} has been marked'})
+      else:
+          emit('error_occurred', {'message': f'Note with ID {note_id} not found or not owned by the user'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('edit_note')
 def handle_edit_note(data):
-    note_id = data.get('note_id')
-    new_text = data.get('new_text')
-    user_slug = data.get('user_slug')
+  note_id = data.get('note_id')
+  new_text = data.get('new_text')
+  user_slug = data.get('user_slug')
 
-    if note_id and new_text and user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        encoded_new_text = base64.b64encode(new_text.encode('utf-8')).decode('utf-8') 
-        note = Note.query.filter_by(id=note_id, user_slug=hashed_slug).first()
+  if note_id and new_text and user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      encoded_new_text = base64.b64encode(new_text.encode('utf-8')).decode('utf-8') 
+      note = Note.query.filter_by(id=note_id, user_slug=hashed_slug).first()
 
-        if note:
-            note.note_content = encoded_new_text
-            db.session.commit()
-            emit('note_edited', {'message': 'Note has been edited'})
-        else:
-            emit('error_occurred', {'message': f'Note with ID {note_id} not found or not owned by the user'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+      if note:
+          note.note_content = encoded_new_text
+          db.session.commit()
+          emit('note_edited', {'message': 'Note has been edited'})
+      else:
+          emit('error_occurred', {'message': f'Note with ID {note_id} not found or not owned by the user'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 #snippets  
 @socketio.on('store_snippet')
 def handle_store_snippet(data):
-    user_slug = data.get('user')
-    title = data.get('title')
-    code = data.get('code')
+  user_slug = data.get('user')
+  title = data.get('title')
+  code = data.get('code')
 
-    if user_slug and title and code:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        new_snippet = Snippet(user_slug=hashed_slug, title=title, code=code)
-        db.session.add(new_snippet)
-        db.session.commit()
-        emit('snippet_stored', {'message': 'Snippet stored successfully'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+  if user_slug and title and code:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      new_snippet = Snippet(user_slug=hashed_slug, title=title, code=code)
+      db.session.add(new_snippet)
+      db.session.commit()
+      emit('snippet_stored', {'message': 'Snippet stored successfully'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('get_snippets')
 def handle_get_snippets(data):
-    user_slug = data.get('user')
+  user_slug = data.get('user')
 
-    if user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        snippets = Snippet.query.filter_by(user_slug=hashed_slug).order_by(Snippet.last_used.desc()).all()  # Update this line
+  if user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      snippets = Snippet.query.filter_by(user_slug=hashed_slug).order_by(Snippet.last_used.desc()).all()  # Update this line
 
-        snippets_list = [{
-            'id': snippet.id,
-            'title': snippet.title,
-            'code': snippet.code
-        } for snippet in snippets]
+      snippets_list = [{
+          'id': snippet.id,
+          'title': snippet.title,
+          'code': snippet.code
+      } for snippet in snippets]
 
-        emit('snippets_fetched', snippets_list)
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+      emit('snippets_fetched', snippets_list)
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('delete_snippet')
 def handle_delete_snippet(data):
-    snippet_id = data.get('snippet_id')
-    user_slug = data.get('user')
+  snippet_id = data.get('snippet_id')
+  user_slug = data.get('user')
 
-    if snippet_id and user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        snippet = Snippet.query.filter_by(id=snippet_id, user_slug=hashed_slug).first()
+  if snippet_id and user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      snippet = Snippet.query.filter_by(id=snippet_id, user_slug=hashed_slug).first()
 
-        if snippet:
-            db.session.delete(snippet)
-            db.session.commit()
-            emit('snippet_deleted', {'message': f'Snippet with ID {snippet_id} has been deleted'})
-        else:
-            emit('error_occurred', {'message': f'Snippet with ID {snippet_id} not found or not owned by the user'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+      if snippet:
+          db.session.delete(snippet)
+          db.session.commit()
+          emit('snippet_deleted', {'message': f'Snippet with ID {snippet_id} has been deleted'})
+      else:
+          emit('error_occurred', {'message': f'Snippet with ID {snippet_id} not found or not owned by the user'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('edit_snippet_code')
 def handle_edit_snippet_code(data):
-    snippet_id = data.get('snippet_id')
-    new_code = data.get('new_code')
-    user_slug = data.get('user')
-    if snippet_id and new_code and user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        snippet = Snippet.query.filter_by(id=snippet_id, user_slug=hashed_slug).first()
+  snippet_id = data.get('snippet_id')
+  new_code = data.get('new_code')
+  user_slug = data.get('user')
+  if snippet_id and new_code and user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      snippet = Snippet.query.filter_by(id=snippet_id, user_slug=hashed_slug).first()
 
-        if snippet:
-            snippet.code = new_code
-            db.session.commit()
-            emit('snippet_code_edited', {'message': 'Snippet content has been edited'})
-        else:
-            emit('error_occurred', {'message': f'Snippet with ID {snippet_id} not found or not owned by the user'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+      if snippet:
+          snippet.code = new_code
+          db.session.commit()
+          emit('snippet_code_edited', {'message': 'Snippet content has been edited'})
+      else:
+          emit('error_occurred', {'message': f'Snippet with ID {snippet_id} not found or not owned by the user'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 @socketio.on('update_snippet_last_used')
 def handle_update_snippet_last_used(data):
-    snippet_id = data.get('snippet_id')
-    user_slug = data.get('user')
+  snippet_id = data.get('snippet_id')
+  user_slug = data.get('user')
 
-    if snippet_id and user_slug:
-        hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
-        snippet = Snippet.query.filter_by(id=snippet_id, user_slug=hashed_slug).first()
+  if snippet_id and user_slug:
+      hashed_slug = hash_user_slug(user_slug, USER_SLUG_SALT)
+      snippet = Snippet.query.filter_by(id=snippet_id, user_slug=hashed_slug).first()
 
-        if snippet:
-            snippet.last_used = datetime.utcnow()
-            db.session.commit()
-            emit('snippet_last_used_updated', {'message': 'Snippet last used updated'})
-        else:
-            emit('error_occurred', {'message': f'Snippet with ID {snippet_id} not found or not owned by the user'})
-    else:
-        emit('error_occurred', {'message': 'Invalid input'})
+      if snippet:
+          snippet.last_used = datetime.utcnow()
+          db.session.commit()
+          emit('snippet_last_used_updated', {'message': 'Snippet last used updated'})
+      else:
+          emit('error_occurred', {'message': f'Snippet with ID {snippet_id} not found or not owned by the user'})
+  else:
+      emit('error_occurred', {'message': 'Invalid input'})
 
 if __name__ == "__main__":
   with app.app_context():
@@ -236,5 +237,5 @@ if __name__ == "__main__":
   now = datetime.now()
   current_time = now.strftime("%H:%M:%S")
   print(f"Running! Started at {current_time}")
-  http_server = WSGIServer(('0.0.0.0', 8080), app, handler_class=WebSocketHandler)
+  http_server = WSGIServer(('0.0.0.0', randint(1111,9999)), app, handler_class=WebSocketHandler)
   http_server.serve_forever()
